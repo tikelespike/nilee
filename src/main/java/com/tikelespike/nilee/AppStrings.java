383919f3794c40073be46ac02876ed265870b453
Timo Weberruß <timo.weberruss@student.kit.edu>
2023-02-18 21:49:58 +0100

    Cleanup and minor polishes

--- src/main/java/com/tikelespike/nilee/AppStrings.java
+++ src/main/java/com/tikelespike/nilee/AppStrings.java
@@ -3,6 +3,18 @@ package com.tikelespike.nilee;
 public class AppStrings {
 
     public static final String APP_NAME = "Nilee";
+    public static final String CHARACTER_NAME_HEADER = "Name";
     public static final String OPEN_CHARACTER_HEADER = "Character Sheet";
     public static final String OPEN_CHARACTER_CTA = "Open";
+    public static final String DEL_CHARACTER_HEADER = "Delete Character";
+    public static final String DEL_CHARACTER_CTA = "Delete";
+
+    /**
+     * First parameter is the character name.
+     */
+    public static final String DEL_CHARACTER_CONFIRMATION_BODY = "Are you sure you want to delete the character \"%s\"?";
+    public static final String CANCEL_CTA = "Cancel";
+    public static final String ADD_CHARACTER_CTA = "Add new character";
+    public static final String CHARACTER_ID_PARAM = "id";
+    public static final String CHARACTER_NOT_FOUND = "Invalid or unknown character";
 }
--- src/main/java/com/tikelespike/nilee/views/mainmenu/CharacterSelectionView.java
+++ src/main/java/com/tikelespike/nilee/views/mainmenu/CharacterSelectionView.java
@@ -9,78 +9,165 @@ import com.tikelespike.nilee.security.AuthenticatedUser;
 import com.tikelespike.nilee.views.sheet.CharacterSheetView;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.KeyModifier;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
-import com.vaadin.flow.component.messages.MessageInput;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteParam;
-import com.vaadin.flow.router.RouteParameters;
 
 import javax.annotation.security.PermitAll;
+import java.util.ArrayList;
+import java.util.List;
 
 @PageTitle("My Characters")
 @Route(value = "characters", layout = MainLayout.class)
 @PermitAll
 public class CharacterSelectionView extends VerticalLayout {
 
-    private TextField newCharacterName;
-    private Button addCharacterButton;
+    private final TextField newCharacterNameTF;
 
-    private Grid<PlayerCharacter> characterGrid;
+    private final Grid<PlayerCharacter> characterGrid;
 
     private final User currentUser;
+    private final UserService userService;
+    private final PlayerCharacterService characterService;
 
 
     public CharacterSelectionView(AuthenticatedUser authenticatedUser, PlayerCharacterService playerCharacterService, UserService userService) {
         if (authenticatedUser.get().isEmpty()) {
             throw new IllegalStateException("User not authenticated");
         }
-        currentUser = authenticatedUser.get().get();
+        this.currentUser = authenticatedUser.get().get();
+        this.userService = userService;
+        this.characterService = playerCharacterService;
 
-        initComponents(playerCharacterService, userService);
+        this.characterGrid = createCharacterGrid();
+        updateCharacterGrid();
 
-        var addCharacterLayout = new HorizontalLayout(newCharacterName, addCharacterButton);
+        this.newCharacterNameTF = new TextField(AppStrings.CHARACTER_NAME_HEADER);
+        newCharacterNameTF.addFocusShortcut(Key.KEY_N, KeyModifier.CONTROL, KeyModifier.ALT);
+        Button addCharacterButton = createAddCharacterButton();
+        var addCharacterLayout = new HorizontalLayout(newCharacterNameTF, addCharacterButton);
         addCharacterLayout.setVerticalComponentAlignment(Alignment.END, addCharacterButton);
+
         add(addCharacterLayout, characterGrid);
     }
 
-    private void initComponents(PlayerCharacterService playerCharacterService, UserService userService) {
-        newCharacterName = new TextField("Name");
-        addCharacterButton = new Button("Add new Character");
-        addCharacterButton.addClickListener(e -> {
-            PlayerCharacter newCharacter = new PlayerCharacter();
-            newCharacter.setName(newCharacterName.getValue());
-            newCharacter.setDex(10);
-            newCharacter.setOwner(currentUser);
-            currentUser.getCharacters().add(newCharacter);
-            playerCharacterService.update(newCharacter);
-            userService.update(currentUser);
-            updateCharacterGrid();
+    private Grid<PlayerCharacter> createCharacterGrid() {
+        Grid<PlayerCharacter> grid = new Grid<>(PlayerCharacter.class, false);
+        grid.addColumn(PlayerCharacter::getName).setHeader(AppStrings.CHARACTER_NAME_HEADER).setAutoWidth(true).setFlexGrow(0);
+        grid.addColumn(new ComponentRenderer<>(this::createOpenPCButton)).setHeader(AppStrings.OPEN_CHARACTER_HEADER);
+        grid.addColumn(new ComponentRenderer<>(this::createDeletePCButton)).setHeader(AppStrings.DEL_CHARACTER_HEADER);
+        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
+        grid.addSelectionListener(e -> {
+            if (e.getFirstSelectedItem().isPresent()) {
+                PlayerCharacter pc = e.getFirstSelectedItem().get();
+                openPCSheet(pc);
+            }
         });
-        characterGrid = new Grid<>(PlayerCharacter.class, false);
-        characterGrid.addColumn(PlayerCharacter::getName).setHeader("Name").setAutoWidth(true).setFlexGrow(0);
-        characterGrid.addColumn(new ComponentRenderer<>(this::createOpenCharacterButton)).setHeader(AppStrings.OPEN_CHARACTER_HEADER);
-        characterGrid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
+        return grid;
+    }
+
+    private Button createAddCharacterButton() {
+        Button button = new Button(AppStrings.ADD_CHARACTER_CTA);
+        button.addClickShortcut(Key.ENTER);
+        button.addClickListener(e -> addPC(createNewCharacter(newCharacterNameTF.getValue())));
+        return button;
+    }
+
+    private PlayerCharacter createNewCharacter(String name) {
+        PlayerCharacter newCharacter = new PlayerCharacter();
+        newCharacter.setName(name);
+        newCharacter.setDex(10);
+        newCharacter.setOwner(currentUser);
+        return newCharacter;
+    }
+
+    private void addPC(PlayerCharacter character) {
+        characterService.update(character);
+        currentUser.getCharacters().add(character);
+        userService.update(currentUser);
         updateCharacterGrid();
     }
 
-    private Button createOpenCharacterButton(PlayerCharacter pc) {
+    private Button createOpenPCButton(PlayerCharacter pc) {
         Button openButton = new Button(AppStrings.OPEN_CHARACTER_CTA);
-        ComponentEventListener<ClickEvent<Button>> navigateToCharacterSheet = e -> {
-            RouteParameters params = new RouteParameters(new RouteParam("id", pc.getId().toString()));
-            openButton.getUI().ifPresent(ui -> ui.navigate(CharacterSheetView.class, pc.getId()));
-        };
+        ComponentEventListener<ClickEvent<Button>> navigateToCharacterSheet = e -> openPCSheet(pc);
         openButton.addClickListener(navigateToCharacterSheet);
         return openButton;
     }
 
+    private void openPCSheet(PlayerCharacter pc) {
+        ensureSanity(pc);
+        getUI().ifPresent(ui -> ui.navigate(CharacterSheetView.class, pc.getId()));
+    }
+
+    private Button createDeletePCButton(PlayerCharacter pc) {
+        Button deleteButton = new Button();
+        deleteButton.addThemeVariants(ButtonVariant.LUMO_ICON, ButtonVariant.LUMO_ERROR, ButtonVariant.LUMO_TERTIARY);
+        deleteButton.setIcon(new Icon(VaadinIcon.TRASH));
+
+        Dialog confirmDialog = createConfirmDialog(pc);
+        deleteButton.addClickListener(e -> confirmDialog.open());
+
+        return deleteButton;
+    }
+
+    private Dialog createConfirmDialog(PlayerCharacter pc) {
+        Dialog confirmDialog = new Dialog();
+        confirmDialog.setHeaderTitle(AppStrings.DEL_CHARACTER_HEADER);
+        confirmDialog.add(String.format(AppStrings.DEL_CHARACTER_CONFIRMATION_BODY, pc.getName()));
+
+        Button confirmButton = new Button(AppStrings.DEL_CHARACTER_CTA);
+        confirmButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_ERROR);
+        confirmButton.addClickListener(e -> {
+            deletePC(pc);
+            confirmDialog.close();
+        });
+
+        Button cancelButton = new Button(AppStrings.CANCEL_CTA);
+        cancelButton.addThemeVariants();
+        cancelButton.addClickListener(e -> confirmDialog.close());
+
+        confirmDialog.getFooter().add(cancelButton, confirmButton);
+        return confirmDialog;
+    }
+
+    private void deletePC(PlayerCharacter pc) {
+        ensureSanity(pc);
+        currentUser.getCharacters().remove(pc);
+        userService.update(currentUser);
+        characterService.delete(pc.getId());
+        updateCharacterGrid();
+    }
+
     private void updateCharacterGrid() {
         characterGrid.setItems(currentUser.getCharacters());
+    }
+
+    private boolean sanityCheck(PlayerCharacter character) {
+        return character != null
+            && character.getOwner() != null
+            && character.getOwner().equals(currentUser)
+            && character.getId() != null
+            && characterService.get(character.getId()).isPresent();
+    }
+
+    private void ensureSanity(PlayerCharacter character) {
+        if (!sanityCheck(character)) {
+            Notification.show(AppStrings.CHARACTER_NOT_FOUND);
+            throw new IllegalStateException("Invalid character (null, not found, or not owned by current user)");
+        }
     }
 }
