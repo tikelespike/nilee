ba8b746d37374151998e2dc914c2b4da69bd36b0
Timo Weberruß <timo.weberruss@student.kit.edu>
2023-03-08 00:03:59 +0100

    Clean up preference view a bit

--- src/main/java/com/tikelespike/nilee/views/mainmenu/UserPreferencesView.java
+++ src/main/java/com/tikelespike/nilee/views/mainmenu/UserPreferencesView.java
@@ -1,12 +1,12 @@
 package com.tikelespike.nilee.views.mainmenu;
 
 import com.tikelespike.nilee.data.entity.User;
-import com.tikelespike.nilee.data.service.UserRepository;
 import com.tikelespike.nilee.data.service.UserService;
 import com.tikelespike.nilee.security.AuthenticatedUser;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.i18n.I18NProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -18,28 +18,51 @@ import java.util.Locale;
 @PageTitle("User Preferences")
 @Route(value = "preferences", layout = MainLayout.class)
 @PermitAll
-public class UserPreferencesView extends FormLayout {
+public class UserPreferencesView extends VerticalLayout {
 
 
     public UserPreferencesView(I18NProvider i18NProvider, UserService userService,
                                AuthenticatedUser authenticatedUser) {
         User user = authenticatedUser.get().orElseThrow(() -> new IllegalStateException("User not authenticated"));
 
-        ComboBox<Locale> localeComboBox = new ComboBox<>("Language");
-        localeComboBox.setItems(i18NProvider.getProvidedLocales());
-        localeComboBox.setItemLabelGenerator(l -> l.getDisplayLanguage(getLocale()));
-        localeComboBox.addValueChangeListener(e -> user.setPreferredLocale(e.getValue()));
-        if (user.getPreferredLocale() != null) {
-            localeComboBox.setValue(user.getPreferredLocale());
-        }
+        FormLayout form = createForm(i18NProvider, userService, user);
+        add(form);
+        expand(form);
 
+        Button saveButton = createSaveButton(userService, user);
+        setHorizontalComponentAlignment(Alignment.STRETCH, saveButton);
+
+        add(saveButton);
+
+        setSizeFull();
+    }
+
+    private FormLayout createForm(I18NProvider i18NProvider, UserService userService, User user) {
+        FormLayout form = new FormLayout();
+        ComboBox<Locale> localeComboBox = createLocaleComboBox(i18NProvider, user);
+        form.addFormItem(localeComboBox, "Language");
+        return form;
+    }
+
+    private Button createSaveButton(UserService userService, User user) {
         Button saveButton = new Button("Save");
         saveButton.addClickListener(e -> {
             userService.update(user);
             VaadinSession.getCurrent().setLocale(user.getPreferredLocale());
             getUI().ifPresent(ui -> ui.getPage().reload());
         });
+        return saveButton;
+    }
 
-        add(localeComboBox, saveButton);
+    private ComboBox<Locale> createLocaleComboBox(I18NProvider i18NProvider, User user) {
+        ComboBox<Locale> localeComboBox = new ComboBox<>();
+        localeComboBox.setItems(i18NProvider.getProvidedLocales());
+        localeComboBox.setItemLabelGenerator(l -> l.getDisplayLanguage(getLocale()));
+        localeComboBox.addValueChangeListener(e -> user.setPreferredLocale(e.getValue()));
+        if (user.getPreferredLocale() != null) {
+            localeComboBox.setValue(user.getPreferredLocale());
+        }
+        localeComboBox.addThemeVariants();
+        return localeComboBox;
     }
 }
