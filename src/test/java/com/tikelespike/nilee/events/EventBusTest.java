c065bd16c021c5ae1892b141f86a3b91da2b9f85
Timo Weberruß <timo.weberruss@student.kit.edu>
2023-03-11 17:34:26 +0100

    Add more event bus tests

--- src/test/java/com/tikelespike/nilee/events/EventBusTest.java
+++ src/test/java/com/tikelespike/nilee/events/EventBusTest.java
@@ -6,19 +6,77 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EventBusTest {
+
+    // CUT
     private EventBus bus;
+
+    private SimpleListener simpleListener;
+    private MessageListener messageListener;
 
     @BeforeEach
     public void setUp() {
         bus = new EventBus();
+        simpleListener = new SimpleListener();
+        messageListener = new MessageListener();
     }
 
     @Test
     public void testSingleListenerSingleEvent() {
-        GenTestListener listener = new GenTestListener();
-        bus.registerListener(Event.class, listener);
+        bus.registerListener(Event.class, simpleListener);
         bus.fireEvent(new Event());
-        assertEquals(listener.getNumCalls(), 1, "Listener should have been called");
+        assertEquals(simpleListener.getNumCalls(), 1, "Listener should have been called");
+    }
+
+    @Test
+    public void testNotCalledOnUnsubscribedEvent() {
+        bus.registerListener(TestEvent.class, simpleListener);
+        bus.fireEvent(new Event());
+        assertEquals(simpleListener.getNumCalls(), 0, "Listener should not have been called by unsubscribed event type");
+    }
+
+    @Test
+    public void testCalledOnSubclassEvent() {
+        bus.registerListener(Event.class, simpleListener);
+        bus.fireEvent(new TestEvent("test"));
+        assertEquals(simpleListener.getNumCalls(), 1, "Listener should have been called because it subscribed to all events, specifically the subclass");
+    }
+
+    @Test
+    public void testMultipleListeners() {
+        bus.registerListener(TestEvent.class, simpleListener);
+        bus.registerListener(TestEvent.class, messageListener);
+        bus.fireEvent(new TestEvent("test"));
+        assertEquals(simpleListener.getNumCalls(), 1, "Listener should have been called because it subscribed to all events");
+        assertEquals(messageListener.getNumCalls(), 1, "Listener should have been called because it subscribed to the specific event");
+        assertEquals(messageListener.getLastMessage(), "test", "Listener should have received the correct message");
+    }
+
+    @Test
+    public void testMultipleRegistration() {
+        bus.registerListener(Event.class, simpleListener);
+        bus.registerListener(Event.class, simpleListener);
+        bus.fireEvent(new Event());
+        assertEquals(simpleListener.getNumCalls(), 2, "Listener should have been called twice because it was registered twice");
+    }
+
+    @Test
+    public void testUnsubscribe() {
+        Registration registration = bus.registerListener(Event.class, simpleListener);
+        registration.unregister();
+        bus.fireEvent(new Event());
+        assertEquals(simpleListener.getNumCalls(), 0, "Listener should not have been called because unregistered before event");
+    }
+
+    @Test
+    public void testUnsubscribeMultiple() {
+        Registration registration = bus.registerListener(Event.class, simpleListener);
+        Registration registration2 = bus.registerListener(Event.class, simpleListener);
+        registration.unregister();
+        bus.fireEvent(new Event());
+        assertEquals(simpleListener.getNumCalls(), 1, "Listener should have been called once because one of two registrations was unregistered");
+        registration2.unregister();
+        bus.fireEvent(new Event());
+        assertEquals(simpleListener.getNumCalls(), 1, "Listener should not have been called because unregistered before event");
     }
 
     private class TestEvent extends Event {
@@ -33,7 +91,7 @@ class EventBusTest {
         }
     }
 
-    private class GenTestListener implements EventListener<Event> {
+    private class SimpleListener implements EventListener<Event> {
         private int numCalls = 0;
 
         @Override
@@ -46,7 +104,7 @@ class EventBusTest {
         }
     }
 
-    private class TestEventListener implements EventListener<TestEvent> {
+    private class MessageListener implements EventListener<TestEvent> {
         private int numCalls = 0;
         private String lastMessage;
 
