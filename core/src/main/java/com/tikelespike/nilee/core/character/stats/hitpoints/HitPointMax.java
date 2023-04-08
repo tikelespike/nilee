3cf17887f20a41f7be3b647e701ef992ebe76c26
Timo Weberruß <timo.weberruss@student.kit.edu>
2023-04-09 00:28:50 +0200

    Document HitPointMax class

--- core/src/main/java/com/tikelespike/nilee/core/character/stats/hitpoints/HitPointMax.java
+++ core/src/main/java/com/tikelespike/nilee/core/character/stats/hitpoints/HitPointMax.java
@@ -6,6 +6,10 @@ import com.tikelespike.nilee.core.property.OverridableProperty;
 import com.tikelespike.nilee.core.property.Property;
 import com.tikelespike.nilee.core.property.convenience.MaxValueSelector;
 
+/**
+ * The property describing the upper limit of hit points a character can have. By default, this value is calculated
+ * based on the constitution score and the class hit die.
+ */
 public class HitPointMax extends OverridableProperty<Integer> {
 
     private Property<Integer> base;
@@ -21,15 +25,16 @@ public class HitPointMax extends OverridableProperty<Integer> {
         setBaseValueSelector(new MaxValueSelector<>());
     }
 
+    /**
+     * Creates a new hit point max property based on the given constitution score.
+     *
+     * @param constitution the CON modifier of this property is used to calculate the default base value of this property
+     */
     public HitPointMax(AbilityScore constitution) {
         this();
         abilityScoreBaseSupplier = new AbilityScoreBaseSupplier(constitution);
         base = new Property<>(abilityScoreBaseSupplier);
         hpMaxBaseSupplier = new HPMaxBaseSupplier(base);
         addBaseValueSupplier(hpMaxBaseSupplier);
-    }
-
-    public Property<Integer> getBaseValueProperty() {
-        return base;
     }
 }
