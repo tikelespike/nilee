6c9c378ed2ca42f34fda54f14103daafcf8b4272
Timo Weberruß <timo.weberruss@student.kit.edu>
2023-04-11 16:19:18 +0200

    Document manual override modifier

--- core/src/main/java/com/tikelespike/nilee/core/property/convenience/ManualOverrideModifier.java
+++ core/src/main/java/com/tikelespike/nilee/core/property/convenience/ManualOverrideModifier.java
@@ -2,19 +2,38 @@ package com.tikelespike.nilee.core.property.convenience;
 
 import com.tikelespike.nilee.core.property.PropertyModifier;
 
+/**
+ * A {@link PropertyModifier} that overrides the value of the property it is applied to with a fixed value, usually set
+ * manually by the user.
+ *
+ * @param <T> the type of the property to override
+ */
 public class ManualOverrideModifier<T> extends PropertyModifier<T> {
 
     private T overrideValue;
 
+    /**
+     * Creates a new {@link ManualOverrideModifier} with the given override value.
+     *
+     * @param overrideValue the value to override the property with
+     */
     public ManualOverrideModifier(T overrideValue) {
         this.overrideValue = overrideValue;
     }
 
+    /**
+     * Sets the override value of this modifier.
+     *
+     * @param overrideValue the value to override the property with
+     */
     public void setOverrideValue(T overrideValue) {
         this.overrideValue = overrideValue;
         update();
     }
 
+    /**
+     * @return the override value of this modifier
+     */
     public T getOverrideValue() {
         return overrideValue;
     }
