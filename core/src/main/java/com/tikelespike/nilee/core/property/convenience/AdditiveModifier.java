b4f04fff79db4dd5c9e8f1ed4cc2dfde83a9c947
Timo Weberruß <timo.weberruss@student.kit.edu>
2023-04-11 14:04:48 +0200

    Document AdditiveModifier

--- core/src/main/java/com/tikelespike/nilee/core/property/convenience/AdditiveModifier.java
+++ core/src/main/java/com/tikelespike/nilee/core/property/convenience/AdditiveModifier.java
@@ -2,15 +2,20 @@ package com.tikelespike.nilee.core.property.convenience;
 
 import com.tikelespike.nilee.core.property.PropertyModifier;
 
+/**
+ * A {@link PropertyModifier} that adds a fixed value to the value of the property it is applied to.
+ */
 public class AdditiveModifier extends PropertyModifier<Integer> {
 
     private int bonus;
     private String source;
 
-    public AdditiveModifier() {
-        this(0, "");
-    }
-
+    /**
+     * Creates a new {@link AdditiveModifier} with the given bonus and source.
+     *
+     * @param bonus how much to add to the value of the property. Can be negative.
+     * @param source a short name describing the source of the bonus, e.g. "Racial bonus"
+     */
     public AdditiveModifier(int bonus, String source) {
         this.bonus = bonus;
         this.source = source;
@@ -36,20 +41,40 @@ public class AdditiveModifier extends PropertyModifier<Integer> {
         return source;
     }
 
+    /**
+     * Sets the offset applied by this modifier.
+     *
+     * @param offset how much to add to the value of the property. Can be negative.
+     */
     public void setBonus(int offset) {
         this.bonus = offset;
         update();
     }
 
+    /**
+     * Returns the offset applied by this modifier.
+     *
+     * @return how much this modifier adds to the value of the property. Can be negative.
+     */
     public int getBonus() {
         return bonus;
     }
 
+    /**
+     * Sets the source description of this modifier. This is used to identify the source of the modifier in the UI.
+     *
+     * @param source a short description for the source of the bonus, e.g. "Racial bonus"
+     */
     public void setSource(String source) {
         this.source = source;
         update();
     }
 
+    /**
+     * Gets the source description of this modifier. This is used to identify the source of the modifier in the UI.
+     *
+     * @return a short description for the source of the bonus, e.g. "Racial bonus"
+     */
     public String getSource() {
         return source;
     }
