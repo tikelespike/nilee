f3b2a2e03eebd1f622161a96858e98db33af66af
Timo Weberruß <timo.weberruss@student.kit.edu>
2023-04-11 23:22:29 +0200

    Document multiplicative modifier

--- core/src/main/java/com/tikelespike/nilee/core/property/convenience/MultiplicativeModifier.java
+++ core/src/main/java/com/tikelespike/nilee/core/property/convenience/MultiplicativeModifier.java
@@ -2,15 +2,20 @@ package com.tikelespike.nilee.core.property.convenience;
 
 import com.tikelespike.nilee.core.property.PropertyModifier;
 
+/**
+ * A {@link PropertyModifier} that multiplies the value by a given factor.
+ */
 public class MultiplicativeModifier extends PropertyModifier<Integer> {
 
     private int factor;
     private String source;
 
-    public MultiplicativeModifier() {
-        this(1, "");
-    }
-
+    /**
+     * Creates a new {@link MultiplicativeModifier} with the given factor and source.
+     *
+     * @param factor how much to multiply the value of the property by. Can be negative.
+     * @param source a short description of the source of the factor (e.g. "Feline agility" doubling movement speed)
+     */
     public MultiplicativeModifier(int factor, String source) {
         this.factor = factor;
         this.source = source;
@@ -36,21 +41,26 @@ public class MultiplicativeModifier extends PropertyModifier<Integer> {
         return source;
     }
 
+    /**
+     * @param factor how much to multiply the value of the property by. Can be negative.
+     */
     public void setFactor(int factor) {
         this.factor = factor;
         update();
     }
 
+    /**
+     * @return the factor this modifier multiplies the property by
+     */
     public int getFactor() {
         return factor;
     }
 
+    /**
+     * @param source a short description of the source of the modifier (e.g. "Feline agility" doubling movement speed)
+     */
     public void setSource(String source) {
         this.source = source;
         update();
-    }
-
-    public String getSource() {
-        return source;
     }
 }
