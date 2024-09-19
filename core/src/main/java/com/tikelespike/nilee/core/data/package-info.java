12352637ca3e5765432780d6b613999d8767beed
Timo Weberruß <timo.weberruss@student.kit.edu>
2024-09-19 11:32:10 +0200

    Add package-info files for core.data

---
+++ core/src/main/java/com/tikelespike/nilee/core/data/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains code for accessing the database. Currently, persistence is implemented using JPA, and persistent data
+ * includes application users as well as most information about their characters (the character models are not stored in
+ * this package but in the {@link com.tikelespike.nilee.core.character} package).
+ */
+package com.tikelespike.nilee.core.data;
